fa3ea4a51886f6f3078fe11da5d9430d03008ee2
e <1311018805@qq.com>
2022-03-19 20:48:10 +0800

    TIM例程增加

---
+++ 3.TIM/AutoTimer/Hardware/global.c
@@ -0,0 +1,18 @@
+#include "global.h"
+#include <stdio.h>
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif 
+PUTCHAR_PROTOTYPE
+{
+	while ((USART1->SR & 0X40) == 0); //循环发送,直到发送完毕
+    USART1->DR = (uint8_t) ch;
+    return ch;
+}
+
+uint8_t u1rxdata = 0;
+uint8_t rxhead = 0;
+uint8_t rxtail = 0;
+uint8_t RevSta = 0;
